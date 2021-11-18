461595e331428b864d1dc5ba03e20512abc626e6
Jessica Zeitz <jzeitz@umw.edu>
2021-11-18 11:53:06 -0500

    Added main class

---
+++ Project5/Project5.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Project5 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Project5
+{
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("My first git experience!!");
+    }
+}
